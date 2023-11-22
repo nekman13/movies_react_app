3b4be0e5356064f7c2d96fff64ea4ede2660738c
nikita <macbook@MacBook-Pro-nikita.local>
2023-11-22 09:09:12 +0300

    Переход на https и обработка ошибки

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -17,16 +17,20 @@ class Main extends React.Component {
 
 
     componentDidMount() {
-        fetch(`http://www.omdbapi.com/?apikey=${this.API_KEY}&s=matrix&`)
+        fetch(`https://www.omdbapi.com/?apikey=${this.API_KEY}&s=matrix&`)
             .then(response => response.json())
-            .then(data => this.setState({movies: data.Search, loading: false}));
+            .then(data => this.setState({movies: data.Search, loading: false}))
+            .catch((err) => {
+                console.error(err);
+                this.setState({loading: false});
+        } )
     }
 
 
     searchMovies = (str, type = "") => {
         if (str !== "") {
             this.setState({loading: true});
-            fetch(`http://www.omdbapi.com/?apikey=7851dfcb&s=${str}&${type === "" ? "" : `type=${type}&`}`)
+            fetch(`https://www.omdbapi.com/?apikey=7851dfcb&s=${str}&${type === "" ? "" : `type=${type}&`}`)
                 .then(response => response.json())
                 .then(data => this.setState({movies: data.Search, loading: false}));
         }
